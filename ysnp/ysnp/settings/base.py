c9683d3bd8ddbe9c63d2237769d7f81810145537
Marco Rougeth <marco@rougeth.com>
2017-08-28 14:17:53 -0300

    Setup media and static root folders

--- ysnp/ysnp/settings/base.py
+++ ysnp/ysnp/settings/base.py
@@ -167,7 +167,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR('static_root')
+STATIC_ROOT = BASE_DIR('public/static')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+MEDIA_ROOT = BASE_DIR('public/media')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = '/media/'
 
 
 # Celery
