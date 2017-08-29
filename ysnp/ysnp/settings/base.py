13f8f5fe7725fc6e4189a9078a306f4efaa472b0
Marco Rougeth <marco@rougeth.com>
2017-08-29 17:12:58 -0300

    New home

--- ysnp/ysnp/settings/base.py
+++ ysnp/ysnp/settings/base.py
@@ -167,6 +167,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR('static'),
+]
 STATIC_ROOT = BASE_DIR('staticfiles')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#media-root
